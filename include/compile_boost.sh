77c368ec154e66705e35eadcdf10bf912af41b18
jonasrodriguez <jonas.rodriguez@outlook.com>
2018-12-17 07:10:13 -0800

    Tag correct boost 1.67

--- include/compile_boost.sh
+++ include/compile_boost.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $current_path
+cd boost
+git checkout boost-1.67.0
+
+cd $current_path
 bin_path=$( cd ../bin ; pwd -P)
 rm -rf $bin_path/boost_build
 
