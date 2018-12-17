a316534b667b2ac784f5fed4561ae9f0aa47dcd3
jonasrodriguez <jonas.rodriguez@outlook.com>
2018-12-17 08:18:26 -0800

    Added submodule recurse checkout

--- include/compile_boost.sh
+++ include/compile_boost.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+#Checkout Boost 1.67 tag
 cd $current_path
 cd boost
-git checkout boost-1.67.0
+git checkout --recurse-submodules tags/boost-1.67.0
 
+#Clean compilation directoy
 cd $current_path
 bin_path=$( cd ../bin ; pwd -P)
 rm -rf $bin_path/boost_build
 
+#Compile boost
 cd boost
 ./bootstrap.sh --prefix=$bin_path/boost_build --with-libraries=thread 
 ./b2 install
