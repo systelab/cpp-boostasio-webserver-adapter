943222dba386ae4ab1eb3002bcfc8f772107991e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-04-02 23:19:33 +0200

    Added tests for mutual SSL

--- test/BoostAsioWebServerAdapterTest/Tests/SecuredServerTest.cpp
+++ test/BoostAsioWebServerAdapterTest/Tests/SecuredServerTest.cpp
@@ -24,9 +24,9 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 		void SetUp()
 		{
 			setUpCertificates();
-			setUpSecuredServer();
+			setUpSecuredServer(false);
 			setUpWebService();
-			setUpHttpsClient();
+			setUpHttpsClient(false);
 		}
 
 		void setUpCertificates()
@@ -91,9 +91,63 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 				"aEfu9/0TYAG7UvJvrLjcmzhU+pDKQQn/DkqnExo5OSQjduxlZU37apaF+LxLCKqN\n"
 				"9S4O7LL7IP4h7Eb8kStGB0m0RppNJZhU/06Xt7kDlryl6+Dv4pkLAgEC\n"
 				"-----END DH PARAMETERS-----\n";
+
+			m_clientCertificate =
+				"-----BEGIN CERTIFICATE-----\n"
+				"MIIDzjCCArYCCQC9BH7ZmAptXjANBgkqhkiG9w0BAQsFADCBqDELMAkGA1UEBhMC\n"
+				"RVMxEjAQBgNVBAgMCUJhcmNlbG9uYTESMBAGA1UEBwwJQmFyY2Vsb25hMSMwIQYD\n"
+				"VQQKDBpTeXN0ZWxhYiBUZWNobm9sb2dpZXMgUy5BLjENMAsGA1UECwwEQWxiYTEU\n"
+				"MBIGA1UEAwwLQWxiYSBDbGllbnQxJzAlBgkqhkiG9w0BCQEWGGx1aXMucnVpekBz\n"
+				"eXN0ZWxhYnN3LmNvbTAeFw0xNjEyMDIxODMwMTNaFw0yNjExMzAxODMwMTNaMIGo\n"
+				"MQswCQYDVQQGEwJFUzESMBAGA1UECAwJQmFyY2Vsb25hMRIwEAYDVQQHDAlCYXJj\n"
+				"ZWxvbmExIzAhBgNVBAoMGlN5c3RlbGFiIFRlY2hub2xvZ2llcyBTLkEuMQ0wCwYD\n"
+				"VQQLDARBbGJhMRQwEgYDVQQDDAtBbGJhIENsaWVudDEnMCUGCSqGSIb3DQEJARYY\n"
+				"bHVpcy5ydWl6QHN5c3RlbGFic3cuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A\n"
+				"MIIBCgKCAQEArO9mpUXqNtFbUSfM2AmOopaCXrRnT+TByszYKlwUya82yS74ZfHH\n"
+				"ShetV0K0bqfYJsd7I3qtqesK96ML/IxOixmLl6XTfSX/iltnTvOSGM8BzrJWaxVT\n"
+				"NMyxDdTOSqfSi0RWweRaq9v045HbWupM8vAcxJWUgWG1xjRTeOaiU9eXmxJqb4Vn\n"
+				"gOGLEjdq9ethp2llucaSGrmMNVtErDEg5tIh8OhW3VmlYG+XacIQKxhHHU9wEYJX\n"
+				"VyrHx0h1W0R0CiPfc9c0QL7QdBbEUvM9pb0gae65TSgCJE6RebHsccMTavkFI8NB\n"
+				"2GWGrC/buOTCu8RA7buBtbJHx524KNwGuwIDAQABMA0GCSqGSIb3DQEBCwUAA4IB\n"
+				"AQCohhuXRh2JEBcOT8Np/tb14fkDWe6Qb1x/5mveBKoXDgLoDWD2XmJkU0q/5t7c\n"
+				"sbur5LptcwkzfKh/sYbuyGZfPXEk5ZApsaSlPHjl8NQTE9U/mIvmcZjjUEmevGaR\n"
+				"ElNf8DWfZkQaUo0j13vskps8ZVGDyuVOHAscGwnIlol0qC1ebZUC9YJy28gQfcb+\n"
+				"/owR3v9V+B0EMqp6QMF3Cvg2XvjE7Ey+tu1+ezGx1+WQmfPJ7fyKzEh2AXZFkp+w\n"
+				"j4QyZFY/hU+JJFbWfeGMVX8wu+Ip9KXvGRsnlVipO1Xo2GBVLqPo6i4/SNcvk0Zp\n"
+				"F78/5ToWhDdN72/VOS+NXfnT\n"
+				"-----END CERTIFICATE-----\n";
+
+			m_clientPrivateKey =
+				"-----BEGIN RSA PRIVATE KEY-----\n"
+				"MIIEogIBAAKCAQEArO9mpUXqNtFbUSfM2AmOopaCXrRnT+TByszYKlwUya82yS74\n"
+				"ZfHHShetV0K0bqfYJsd7I3qtqesK96ML/IxOixmLl6XTfSX/iltnTvOSGM8BzrJW\n"
+				"axVTNMyxDdTOSqfSi0RWweRaq9v045HbWupM8vAcxJWUgWG1xjRTeOaiU9eXmxJq\n"
+				"b4VngOGLEjdq9ethp2llucaSGrmMNVtErDEg5tIh8OhW3VmlYG+XacIQKxhHHU9w\n"
+				"EYJXVyrHx0h1W0R0CiPfc9c0QL7QdBbEUvM9pb0gae65TSgCJE6RebHsccMTavkF\n"
+				"I8NB2GWGrC/buOTCu8RA7buBtbJHx524KNwGuwIDAQABAoIBAErspgIxGlqJthCh\n"
+				"N8TZzyxuHSm+64vXxoaA3Z7sqiz/drMPYJt6dyeIvpcx1U6j9ahHgPhMLHWT2fqU\n"
+				"b5sJhvYJKUuYktqD3gUpuMaiDMgm19efuIVIhfY4QPWHH6GEuFmSUCxNLWOMNrtp\n"
+				"9CmdnZ0bnrslO2Qa21B2VzvoYKgF56mRfRwo2znwNBTdrmweAgEYyRtT7OzJosYK\n"
+				"/kljzpb54BSBeW1pw/kzVZNdX3a2jtq7XGTnzsWOponxsa0yaZv1uFjVhQppuOkY\n"
+				"MpF/4Un3rDiFxHt85qCpDDVTUjKF0nJsnXGqA1l4cGvq9mF5h1g/6qcQ5adG4hLr\n"
+				"DXZzWnECgYEA3IQIbhOLcz9E+SMMETqIiX9m0OoTaQNzLjRqk5YyqGPWtquNczLV\n"
+				"0NowI+DnZR4oM7eUV2uPj5wWNND55iQpf3+D+8YlCq5e1f7oxBoPa+6oSz0Q4yV2\n"
+				"wodJxj/EEl3BslwNEji+m3UKThsCAhN+z+RCEh2iw9QQLOuAicejFy8CgYEAyMNS\n"
+				"/bueSPs86QThyVVt7tSfgwNDEkrE5swSkiO7yeakAi1XKEUOaVVnzMS/c1AbUgLE\n"
+				"97gtR2KH0MnWgYVc/8WmN0Bw/vZ7SOzwJ3sG9NfDwl27uqB+gWXhPre+EkkE7DV2\n"
+				"544tKJ6luuYmvi3v7AJ8Qp8EhXnM3VYk7nX85jUCgYAKvzyR8M3pDfpUkgqyhEHH\n"
+				"QbhHEkFj6tEgPVm8W6OYZVU/jdjU9nNJPHkAJqPgX7nh216VbB6RBzBl3pFvT+/d\n"
+				"0N8j1CJWBO2GTGgXtpVt0zHMiOtbxqWlEWbdDJqHsp4QDUBt1OolNW0NJW/RnhWe\n"
+				"GCfILj2SgbFQHyIilOIz7QKBgBpfViqQl9Vcc37krkhFWaCS+7U5G3luqvjqe9II\n"
+				"saVvD8JddGKM17zMhEso2UFrl3spc/hakFAdSFwL94qiGzpaG2CMrYtAk6dbwJxf\n"
+				"E+Xb5hJFP+DfLl8PJAyaFHTQdL4Ws9ztF0+Lr5einhpDbI3jMbGKtAxYmKnGpClT\n"
+				"BHq1AoGAYTnf0SUrruRdA02fV/pdPaC2fOKS7HgxGwXqSyUYoZah4in9e/y6ZEU3\n"
+				"rCru1RPkhr4kovoynd2+hnremKqHeglhTZ7QEiJGK5hSmZNiQv9U1M3Vak/4cTV3\n"
+				"z+aXsea1uDV7p3Vg7rrbgAawZz7Vc+lfUWmqFinUAFG/c+yKLQU=\n"
+				"-----END RSA PRIVATE KEY-----\n";
 		}
 
-		void setUpSecuredServer()
+		void setUpSecuredServer(bool mutualSSLEnabled)
 		{
 			m_hostAddress = "127.0.0.1";
 			m_port = 9999;
@@ -108,6 +162,12 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 			securityConfiguration.setServerCertificate(m_serverCertificate);
 			securityConfiguration.setServerPrivateKey(m_serverPrivateKey);
 			securityConfiguration.setServerDHParam(m_serverDHParams);
+
+			if (mutualSSLEnabled)
+			{
+				securityConfiguration.setMutualSSLEnabled(true);
+				securityConfiguration.setClientCertificate(m_clientCertificate);
+			}
 
 			m_securedServer = std::make_unique<SecuredServer>(configuration);
 			m_securedServer->start();
@@ -131,11 +191,19 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 			m_securedServer->registerWebService(std::move(m_webService));
 		}
 
-		void setUpHttpsClient()
+		void setUpHttpsClient(bool mutualSSLEnabled)
 		{
 			SecurityConfiguration securityConfiguration;
 			securityConfiguration.setServerCertificate(m_serverCertificate);
-			m_httpsClient = std::make_unique<HttpsClient>(securityConfiguration, m_hostAddress, std::to_string(m_port));
+
+			if (mutualSSLEnabled)
+			{
+				securityConfiguration.setMutualSSLEnabled(true);
+				securityConfiguration.setClientCertificate(m_clientCertificate);
+			}
+
+			m_httpsClient = std::make_unique<HttpsClient>(securityConfiguration, m_hostAddress,
+														  std::to_string(m_port), m_clientPrivateKey);
 		}
 
 	protected:
@@ -152,10 +220,14 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 		std::string m_serverCertificate;
 		std::string m_serverPrivateKey;
 		std::string m_serverDHParams;
+
+		std::string m_clientCertificate;
+		std::string m_clientPrivateKey;
 	};
 
 
-	TEST_F(SecuredServerTest, testExpectedReplyWithValidCredentials)
+	// HTTPS server
+	TEST_F(SecuredServerTest, testHttpsServerWithValidCredentials)
 	{
 		std::map<std::string, std::string> headers;
 		std::string content;
@@ -173,7 +245,7 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 		}
 	}
 
-	TEST_F(SecuredServerTest, testSetServeExpectedThrowsWithInvalidCredentials)
+	TEST_F(SecuredServerTest, testHttpsServerThrowsExceptionWhenCertifcateIsInvalid)
 	{
 		Configuration configuration;
 		configuration.setHostAddress(m_hostAddress);
@@ -187,6 +259,41 @@ namespace systelab { namespace web_server { namespace boostasio { namespace unit
 		securityConfiguration.setServerDHParam(m_serverDHParams);
 
 		ASSERT_THROW(std::make_unique<SecuredServer>(configuration), systelab::web_server::IServer::Exception);
+	}
+
+
+	// Mutual SSL
+	TEST_F(SecuredServerTest, testHttpsServerWithMutualSSL)
+	{
+		setUpSecuredServer(true);
+		setUpWebService();
+		setUpHttpsClient(true);
+
+		std::map<std::string, std::string> headers;
+		std::string content;
+		m_httpsClient->send("rest/api/users", headers, content);
+
+		std::string expectedContent = m_defaultReply.getContent();
+		EXPECT_EQ(expectedContent, content);
+
+		std::map<std::string, std::string> expectedHeaders = m_defaultReply.getHeaders();
+		EXPECT_EQ(expectedHeaders.size(), headers.size());
+		for (auto expectedHeader : expectedHeaders)
+		{
+			ASSERT_TRUE(headers.find(expectedHeader.first) != headers.end()) << "for header '" << expectedHeader.first << "'";
+			EXPECT_EQ(expectedHeader.second, headers[expectedHeader.first]) << "for header '" << expectedHeader.first << "'";
+		}
+	}
+
+	TEST_F(SecuredServerTest, testHttpsServerWithMutualSSLThrowsExceptionWhenClientHasNotACertificate)
+	{
+		setUpSecuredServer(true);
+		setUpWebService();
+		setUpHttpsClient(false);
+
+		std::map<std::string, std::string> headers;
+		std::string content;
+		ASSERT_THROW(m_httpsClient->send("rest/api/users", headers, content), std::exception);
 	}
 
 }}}}
