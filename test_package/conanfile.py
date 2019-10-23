064814b8f4b77b468aa48c2151216269a226a7fa
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-24 00:46:25 +0200

    Defined options for test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,6 +4,13 @@ from conans import ConanFile, CMake, tools
 class BoostAsioWebServerAdapterTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
+    options = {"gtest": ["1.7.0", "1.8.1"], "boost": ["1.66.0", "1.67.0"], "OpenSSL": ["1.0.2n"]}
+    default_options = {"gtest":"1.8.1", "boost":"1.67.0", "OpenSSL":"1.0.2n"}
+
+    def configure(self):
+        self.options["BoostAsioWebServerAdapter"].gtest = self.options.gtest
+        self.options["BoostAsioWebServerAdapter"].boost = self.options.boost
+        self.options["BoostAsioWebServerAdapter"].OpenSSL = self.options.OpenSSL
 
     def build(self):
         cmake = CMake(self)
