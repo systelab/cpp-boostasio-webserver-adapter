4896116ab6a9dcf7c438beedca44d04493151c30
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-27 23:14:54 +0100

    Added trace to use the server variable

--- test_package/BoostAsioWebServerAdapterExample.cpp
+++ test_package/BoostAsioWebServerAdapterExample.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 	systelab::web_server::boostasio::ServerFactory serverFactory;
 	auto server = serverFactory.buildServer(configuration);
 	std::cout << "Server built on port" << configuration.getPort() << std::endl;
+	std::cout << "Running: " << server->isRunning() << std::endl;
 
     return 0;
 }
