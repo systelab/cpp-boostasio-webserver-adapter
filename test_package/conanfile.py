4b06ba5923d007869ea9d81e5e9a13850a76865b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-27 23:08:16 +0100

    Removed unused import

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 class BoostAsioWebServerAdapterTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
