1a41b4b851b7e5ccee504a4bdc39a0b605e0363d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-05-23 19:28:09 +0200

    Added support for boost 1.71.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, CMake
 class BoostAsioWebServerAdapterTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "boost": ["1.66.0", "1.67.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g"]}
-    default_options = {"gtest":"1.10.0", "boost":"1.67.0", "openssl":"1.1.1g"}
+    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "boost": ["1.66.0", "1.67.0", "1.71.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g"]}
+    default_options = {"gtest":"1.10.0", "boost":"1.71.0", "openssl":"1.1.1g"}
 
     def configure(self):
         self.options["BoostAsioWebServerAdapter"].gtest = self.options.gtest
