adbed37d82bd7885f479bcb62f8ba1356582985c
Joaquim Vila Bou <joaquim.vila@systelabsw.com>
2021-04-01 14:54:02 +0200

    Defined recipe hash for all libraries obtained from conan-center

--- conanfile.py
+++ conanfile.py
@@ -29,23 +29,33 @@ class BoostAsioWebServerAdapterConan(ConanFile):
             self.requires("boost/1.66.0@conan/stable")
         elif self.options.boost == "1.67.0":
             self.requires("boost/1.67.0@conan/stable")
+        elif self.options.boost == "1.72.0":
+            self.requires("boost/1.72.0#50e13743ed3c867fe95f151e113bc1ba")
+        elif self.options.boost == "1.75.0":
+            self.requires("boost/1.75.0#32c81e4e89c54b95b2c2c198fe3cb65f")
         else:
             self.requires(("boost/%s") % self.options.boost)
 
         if self.options.openssl == "1.0.2n":
             self.requires("OpenSSL/1.0.2n@conan/stable")
+        elif self.options.openssl == "1.75.0":
+            self.requires("openssl/1.1.1g#58b78c1738d0cff868861e077e707ca4")
+            self.requires("zlib/1.2.11")
         else:
             self.requires(("openssl/%s") % self.options.openssl)
             self.requires("zlib/1.2.11")
 
     def build_requirements(self):
         self.build_requires("WebServerAdapterTestUtilities/1.1.9@systelab/stable")
+
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
             self.build_requires("gtest/1.8.1")
+        elif self.options.gtest == "1.10.0":
+            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
         else:
-            self.build_requires("gtest/1.10.0")
+            self.build_requires(("gtest/%s") % self.options.gtest)
 
     def build(self):
         cmake = CMake(self)
